b9facc08f0d1e53ea1bd291d7aa228a3bb580876
Isaiah Clifford Opoku <opokuisaiahclifford123@gmail.com>
2024-02-19 22:31:59 +0000

    Adding of model and docker support

---
+++ SupermarketWebAPI/Domain/Models/Category.cs
@@ -0,0 +1,11 @@
+﻿namespace SupermarketWebAPI.Domain.Models
+{
+    public class Category
+    {
+
+        public int id { get; set; }
+        public string name { get; set; } = null!;
+        public List<Product> Products { get; set; } = new();
+
+    }
+}
---
+++ SupermarketWebAPI/Domain/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿
+using SupermarketWebAPI.Domain.Models;
+using System.ComponentModel;
+
+
+
+
+    public class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = null!;
+    public short QuantityInPackage { get; set; }
+    public UnitOfMeasurement UnitOfMeasurement { get; set; }
+
+    public int CategoryId { get; set; }
+    public Category? Category { get; set; }
+}
+
